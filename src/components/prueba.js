812f7b2dc8335c2ddd9cbf69c698544fc1bf2423
A153865 <jfernandezcor@bankia.com>
2018-09-24 20:04:47 +0200

    fixes

--- src/components/prueba.js
+++ src/components/prueba.js
@@ -2,9 +2,20 @@
 import styles from './prueba.scss'
 import {construyeSVG, appendTo, setAtt} from './tools.js'
 const prueba = (w, h, padre)=>{
-    const el = construyeSVG(w, h, styles, 1024, 1024)
-    appendTo(el, {tag: 'rect', x:"20", y:"20", width:"100", height:"280", fill:"blue", stroke:"black", 'stroke-width':"3"})
-    padre.appendChild(el)
+    const svg = construyeSVG(w, h, styles, 1024, 1024)
+    svg.add_h("<def/>").add('<circle id="c" r="50"/>')
+    svg.add_h("<g fill='white'/>")
+    .add({tag: 'rect', x:"20", y:"20", width:"100", height:"280", fill:"blue", stroke:"black", 'stroke-width':"3"})
+    .add(`<circle cx="70" cy="80" r="30" stroke="black" stroke-width="2" />`)
+    svg.add({tag: 'use', 'xlink:href': '#c', x: '120', y: '120', fill: '#40cc40'})
+    svg.add(`<use xlink:href='#c' x='220' y='120' fill='#cc4040'/>`)
+    svg.add_h(`<use x='320' y='120' fill='#cc4040'/>`)
+    .setAtt('xlink:href', '#c')
+    /*const g = appendTo(svg,"<g fill='white'/>")
+    appendTo(g, {tag: 'rect', x:"20", y:"20", width:"100", height:"280", fill:"blue", stroke:"black", 'stroke-width':"3"})
+    appendTo(g , `<circle cx="70" cy="80" r="30" stroke="black" stroke-width="2" />`)*/
+
+    padre.appendChild(svg)
 }
 
 export {prueba}
--- src/components/tools.js
+++ src/components/tools.js
@@ -1,24 +1,54 @@
+const NS ={svg: 'http://www.w3.org/2000/svg', xlink: 'http://www.w3.org/1999/xlink'}
 const construyeSVG = (w, h, styles, vw, vh)=>{
-    const el = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const el = document.createElementNS(NS.svg, 'svg')
     el.setAttribute("width", w)
     el.setAttribute("height", h)
     el.setAttribute("viewBox", `0 0 ${String(vw)} ${String(vh)}`)
-    const tmp = document.createElementNS('http://www.w3.org/2000/svg', 'style')
+    const tmp = document.createElementNS(NS.svg, 'style')
     tmp.setAttribute("type", 'text/css')
-    tmp.appendChild(document.createTextNode(styles))
+    tmp.appendChild(document.createTextNode("/* <![CDATA][ */\n" + styles + "\n/* ]]> */"))
     el.appendChild(tmp)
+    el.setAttribute('xmlns', NS.svg)
+    el.setAttributeNS(NS.svg, 'xlink', NS.xlink)
     return el
 }
 const appendTo = (padre, cfg)=>{
-    const tmp = document.createElementNS('http://www.w3.org/2000/svg', cfg.tag)
+    if (typeof cfg === 'string'){
+        const el = document.createElementNS(NS.svg, 'g')
+        el.innerHTML = cfg
+        const ret = el.childNodes[0]
+        padre.appendChild(ret)
+        return ret
+    }
+    const tmp = document.createElementNS(NS.svg, cfg.tag)
     Object.keys(cfg).forEach(k=>{
-        k!='tag' && tmp.setAttributeNS(null, k, cfg[k])
+        k!='tag' && k.indexOf(':') < 0 && tmp.setAttributeNS(null, k, cfg[k])
+        if (k.indexOf(':')>0){
+            const [ns, prop] = k.split(':')
+            tmp.setAttributeNS(NS[ns], prop, cfg[k]) 
+        } 
     })
     padre.appendChild(tmp)
     return tmp
 }
+!SVGElement.prototype.add_h && (SVGElement.prototype.add_h = function(cfg){
+    return appendTo(this, cfg)
+})
+!SVGElement.prototype.add && (SVGElement.prototype.add = function(cfg){
+    appendTo(this, cfg)
+    return this
+})
 const setAtt = (el, name, value)=>{
+    if (name.indexOf(':')>0){
+        const [ns, prop] = name.split(':')
+        el.setAttributeNS(NS[ns], prop, value) 
+        return el
+    } 
     el.setAttributeNS(null, name, value)
     return el
 }
+!SVGElement.prototype.setAtt && (SVGElement.prototype.setAtt = function(name, value){
+    setAtt(this, name, value)
+    return this
+})
 export {construyeSVG, appendTo, setAtt}
