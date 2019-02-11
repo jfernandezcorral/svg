1f47f0f12770e2a6c5717a46849f2d34982438b9
A153865 <jfernandezcor@bankia.com>
2019-02-11 14:36:16 +0100

    xy

--- i.cmd
+++ i.cmd
@@ -1,3 +1,3 @@
-rem call C:\bin\entorno.cmd
+call C:\bin\entorno.cmd
 start "demo" /d .\ cmd.exe /k yarn demo
 start "build-dev-watch" /d .\ cmd.exe /k yarn build-dev-watch
--- src/components/xy.js
+++ src/components/xy.js
@@ -3,32 +3,43 @@ import {construyeSVG, appendTo, setAtt} from './tools.js'
 const cfgDefault = {
     puntos: [{e: "Enero", v: 25}, {e: "Febrero", v: -5}, {e: "Marzo", v: 10}]
 }
+const calculoRango = (min, max)=>{
+    //
+    if (max-min == 0){
+        return (max==0? [0, 1]: [max-1, max+1])
+    }
+    return [2, 5]
+}
+const h = 1000
+const t = 0.04
+const scale = 0.85
+const pre = 0.13
+const lc = 10
+const htext = 35
 const xy = (padre, cfg=cfgDefault, aspectRatio=16/9)=>{
     const paintMarcas = ()=>{
         const lx = w-(2*offset)
         const inc = lx/(cfg.puntos.length-1)
         let x = offset
         cfg.puntos.forEach(p=>{
-            axis.add(`<line x1="${x}" y1="${h-5}" x2="${x}" y2="${h+5}"/>`)
-            axis.add(`<text x="${x}" y="${h+30}">${p.e}</text>`)
+            axis.add(`<line x1="${x}" y1="${h}" x2="${x}" y2="${h+lc}"/>`)
+            axis.add(`<text x="${x}" y="${h+htext}">${p.e}</text>`)
             x += inc
         })
     }
-    const h = 1000
-    const w = 1000*aspectRatio
-    const t = 0.04
+    
+    const w = h*aspectRatio
     const offset = t*w
     const [min, max] = cfg.puntos.reduce((a, it)=>{
         if (it.v < a[0]){a[0] = it.v}
         if (it.v > a[1]){a[1] = it.v}
         return a
     },[cfg.puntos[0].v, cfg.puntos[0].v])
-    let rango = max - min
-    rango = rango? rango*(1+(2*t)): min? min*(1+(2*t)): 2
-    console.log(rango)
+    const [pmin, pmax] = calculoRango(min, max)
+    console.log(pmin, pmax)
     const svg = construyeSVG(styles, undefined, undefined, w, h)
     //svg.add_h("<def/>").add('<circle id="c" r="50"/>')
-    const axis = svg.add_h(`<g class='axis' transform='scale(.90) translate(${w*0.05}, ${h*0.05})'/>`)
+    const axis = svg.add_h(`<g class='axis' transform='scale(${scale}) translate(${w*pre}, ${h*(1-scale-pre)})'/>`)
     axis.add(`<line y1="${h}" x2="${w}" y2="${h}"/>`)
     axis.add(`<line y2="${h}"/>`)
     paintMarcas()
